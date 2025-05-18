efe0af77e6cb414929840b2a6b2b5251f5ae9fe6
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-05-18 23:31:08 +0530

    java no return

--- solutions/Solution.java
+++ solutions/Solution.java
@@ -12,8 +12,10 @@ public class Solution {
     }
 
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+        //write your code here
       
-        return null;
+        
     }
 
 
