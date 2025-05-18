aa861d7f5158f8b48ce5058443844b536bf936c3
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-05-18 23:44:35 +0530

    java solutions 1

--- solutions/Solution.java
+++ solutions/Solution.java
@@ -12,8 +12,30 @@ public class Solution {
     }
 
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
 
-        //write your code here...
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                current.next = l1;
+                l1 = l1.next;
+            } else {
+                current.next = l2;
+                l2 = l2.next;
+            }
+            current = current.next;
+        }
+
+        if (l1 != null) {
+            current.next = l1;
+        } else {
+            current.next = l2;
+        }
+
+        return dummy.next;
       
         
     }
