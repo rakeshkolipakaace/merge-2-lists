288fb3268969b90d30e677b5cc116e07f77410fb
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-05-18 20:16:46 +0530

    py solutionn

--- solutions/solution.py
+++ solutions/solution.py
@@ -1,23 +1,13 @@
-# solutions/solution.py
-
-class ListNode:
+# Definition for singly-linked list.
+class ListNode(object):
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 
-class Solution:
+class Solution(object):
     def mergeTwoLists(self, list1, list2):
-        dummy = ListNode()
-        current = dummy
-        
-        while list1 and list2:
-            if list1.val < list2.val:
-                current.next = list1
-                list1 = list1.next
-            else:
-                current.next = list2
-                list2 = list2.next
-            current = current.next
-        
-        current.next = list1 if list1 else list2
-        return dummy.next
+        """
+        :type list1: ListNode or None
+        :type list2: ListNode or None
+        :rtype: ListNode or None
+        """
