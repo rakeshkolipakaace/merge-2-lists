adc40b95795c203fb008bf20670f51d1de469df4
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-05-18 20:18:09 +0530

    py solutionnn

--- solutions/solution.py
+++ solutions/solution.py
@@ -11,3 +11,22 @@ class Solution(object):
         :type list2: ListNode or None
         :rtype: ListNode or None
         """
+        dummy = ListNode(-1)  # Temporary node to simplify the merging logic
+        current = dummy
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                current.next = list1
+                list1 = list1.next
+            else:
+                current.next = list2
+                list2 = list2.next
+            current = current.next
+
+        # Attach the remaining elements (if any)
+        if list1:
+            current.next = list1
+        elif list2:
+            current.next = list2
+
+        return dummy.next  # Head of the merged list
